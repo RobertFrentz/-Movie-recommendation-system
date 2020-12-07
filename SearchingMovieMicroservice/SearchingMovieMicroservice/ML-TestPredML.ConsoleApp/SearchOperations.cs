83df6f4cac3663b1982b7705fad2a32ba05bef4f
Alex Cazan <alexgabriel005@gmail.com>
2020-12-07 18:41:11 +0200

    naming solved(I hope so)

---
+++ SearchingMovieMicroservice/SearchingMovieMicroservice/ML-TestPredML.ConsoleApp/SearchOperations.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ML_TestPredML.Model;
+
+namespace ML_TestPredML.ConsoleApp
+{
+    public class SearchOperations
+    {
+
+      
+
+        public static float Prediction(ModelInput data)
+        {
+            var predictionResult = ConsumeModel.Predict(data);
+            return predictionResult.Score;
+        }
+
+        public static List<int> RecommendedMoviesIdByCategory(float userId, List<int> moviesId)
+        {
+            SortedList<float, float> predictions = new SortedList<float, float>();
+            for (int i = 0; i < 15; i++)
+            {
+                ModelInput data = new ModelInput()
+                {
+                    UserId = userId,
+                    MovieId = moviesId[i],
+                };
+                float predict = Prediction(data);
+                if (!predictions.ContainsKey(moviesId[i]))
+                {
+                    predictions.Add(moviesId[i], predict);
+                }
+            }
+            var sortedPredictions = predictions.OrderByDescending(r => r.Value);
+            moviesId.Clear();
+            foreach (KeyValuePair<float, float> prediction in sortedPredictions)
+            {
+                moviesId.Add((int)prediction.Key);
+            }
+            return moviesId;
+        }
+
+        public static List<int> RecommendMoviesIdByKeyword(float userId, List<int> moviesIdTag, List<int> moviesIdTitle)
+        {
+            List<int> moviesIdRecommendedUsingTag = RecommendedMoviesIdByTag(userId, moviesIdTag);
+            List<int> moviesIdRecommendedUsingTitle = RecommendedMoviesIdByTitle(userId,  moviesIdTitle);
+            List<int> finalMoviesId = new List<int>();
+            Random random = new Random();
+            for(int i = 0; i < 15; i++)
+            {
+                int randomNumber = random.Next(2);
+                if(randomNumber == 0)
+                {
+                    if(moviesIdRecommendedUsingTag != null && moviesIdRecommendedUsingTag.Count > i)
+                    {
+                        finalMoviesId.Add(moviesIdRecommendedUsingTag[i]);
+                    }
+                    else
+                    {
+                        if(moviesIdRecommendedUsingTitle != null && moviesIdRecommendedUsingTitle.Count > i)
+                        {
+                            finalMoviesId.Add(moviesIdRecommendedUsingTitle[i]);
+                        }
+            
+                    }
+                    
+                } else
+                {
+                    if(moviesIdRecommendedUsingTitle != null && moviesIdRecommendedUsingTitle.Count > i)
+                    {
+                        finalMoviesId.Add(moviesIdRecommendedUsingTitle[i]);
+                    }
+                    else
+                    {
+                        if(moviesIdRecommendedUsingTag != null && moviesIdRecommendedUsingTag.Count > i)
+                        {
+                            finalMoviesId.Add(moviesIdRecommendedUsingTag[i]);
+                        }
+                        
+                    }
+
+                }
+            }
+            return finalMoviesId;
+        }
+
+        public static List<int> RecommendedMoviesIdByTag(float userId, List<int> moviesId)
+        {
+
+            SortedList<float, float> predictions = new SortedList<float, float>();
+            for (int i = 0; i < moviesId.Count/2; i++)
+            {
+                ModelInput data = new ModelInput()
+                {
+                    UserId = userId,
+                    MovieId = moviesId[i],
+                };
+                float predict = Prediction(data);
+                if (!predictions.ContainsKey(moviesId[i]))
+                {
+                    predictions.Add(moviesId[i], predict);
+                }
+            }
+            var sortedPredictions = predictions.OrderByDescending(r => r.Value);
+            moviesId.Clear();
+            foreach (KeyValuePair<float, float> prediction in sortedPredictions)
+            {
+                moviesId.Add((int)prediction.Key);
+            }
+            return moviesId;
+
+        }
+        public static List<int> RecommendedMoviesIdByTitle(float userId, List<int> moviesId)
+        {
+
+            SortedList<float, float> predictions = new SortedList<float, float>();
+            for (int i = 0; i < moviesId.Count/2; i++)
+            {
+                ModelInput data = new ModelInput()
+                {
+                    UserId = userId,
+                    MovieId = moviesId[i],
+                };
+                float predict = Prediction(data);
+                if (!predictions.ContainsKey(moviesId[i]))
+                {
+                    predictions.Add(moviesId[i], predict);
+                }
+            }
+            var sortedPredictions = predictions.OrderBy(r => r.Value);
+            moviesId.Clear();
+            foreach (KeyValuePair<float, float> prediction in sortedPredictions)
+            {
+                moviesId.Add((int)prediction.Key);
+            }
+            return moviesId;
+        }
+    }
+}
---
+++ SearchingMovieMicroservice/SearchingMovieMicroservice/SearchingMovieMicroservice/Entities/MovieWithTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchingMovieMicroservice.Entities
+{
+    public class MovieWithTag
+    {
+        public int Id { get; set; }
+
+        public int MovieId { get; set; }
+        public string Tag { get; set; }
+    }
+}
