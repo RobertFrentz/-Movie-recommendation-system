74e316bc7a73458a54ea19238d877234987ffd62
Robert Frentz <frentzrobert@yahoo.com>
2020-12-12 12:36:47 +0200

    created user management microservice

---
+++ UserManagementMicroservice/UserManagementMicroservice/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagementMicroservice.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagementMicroservice.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
