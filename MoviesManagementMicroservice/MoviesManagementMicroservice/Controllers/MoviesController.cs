2a02d35a18bc3f018eec3dbdcae38bd9f405f66d
GeorgicaMarius <61226962+GeorgicaMarius@users.noreply.github.com>
2021-01-10 21:21:25 +0200

    Update MoviesController.cs

--- MoviesManagementMicroservice/MoviesManagementMicroservice/Controllers/MoviesController.cs
+++ MoviesManagementMicroservice/MoviesManagementMicroservice/Controllers/MoviesController.cs
@@ -11,7 +11,7 @@ using MoviesManagementMicroservice.Utils;
 
 namespace MoviesManagementMicroservice.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/movies")]
     [ApiController]
     public class MoviesController : ControllerBase
     {
@@ -22,7 +22,8 @@ namespace MoviesManagementMicroservice.Controllers
             _context = context;
         }
 
-        // GET: api/Movies
+        // GET: /api/v1/movies/test
+        [Route("test")]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
         {
@@ -30,8 +31,8 @@ namespace MoviesManagementMicroservice.Controllers
             return await _context.Movies.Take(15).ToListAsync();
         }
 
-        // GET: api/MoviesDetails
-        [Route("HomeMoviesData")]
+        // GET: /api/v1/movies/random
+        [Route("random")]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<int>>> GetHomeMoviesData()
         {
@@ -41,7 +42,8 @@ namespace MoviesManagementMicroservice.Controllers
             return Ok(moviesIds);
         }
 
-        [Route("HomeMoviesData/{movieId}")]
+        // GET: /api/v1/movies/moviedata/summary/{movieId}
+        [Route("moviedata/summary/{movieId}")]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MovieDetails>>> GetHomeMovieDataById(int movieId)
         {
@@ -74,7 +76,8 @@ namespace MoviesManagementMicroservice.Controllers
             return Ok(movieDetails);
         }
 
-        [Route("MoviesData/{movieId}")]
+        // GET: /api/v1/movies/moviedata/complete/{movieId}
+        [Route("moviedata/complete/{movieId}")]
         [HttpGet]
         public async Task<ActionResult<string>> GetMovieDataById(int movieId)
         {
@@ -93,78 +96,6 @@ namespace MoviesManagementMicroservice.Controllers
 
             string json = await DataScraping.GetImdbMovieData(link.ImdbId);
             return Ok(json);
-        }
-
-        // GET: api/Movies/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Movie>> GetMovie(int id)
-        {
-            var movie = await _context.Movies.FindAsync(id);
-
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            return movie;
-        }
-
-        // PUT: api/Movies/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMovie(int id, Movie movie)
-        {
-            if (id != movie.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(movie).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MovieExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Movies
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
-        {
-            _context.Movies.Add(movie);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetMovie", new { id = movie.Id }, movie);
-        }
-
-        // DELETE: api/Movies/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMovie(int id)
-        {
-            var movie = await _context.Movies.FindAsync(id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            _context.Movies.Remove(movie);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
         }
 
         private bool MovieExists(int id)
